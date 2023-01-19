ca6342f1c568379429d6a7f06c24a309b4398d6c
Peter Muriuki <pmuriuki98@gmail.com>
2023-01-19 21:54:49 +0300

    Fix refactored method call

--- apps/web/src/routes/workflows/+page.server.ts
+++ apps/web/src/routes/workflows/+page.server.ts
@@ -1,12 +1,13 @@
 import { getClientSideSymbologyConfigs, pipelineController } from '$lib/server/appConfig';
 import { getLastPipelineMetricForConfig } from '$lib/server/logger/configMetrics';
+import type { ConfigRunner } from '@onaio/symbology-calc-core';
 
 /** @type {import('./$types').PageLoad} */
 export function load() {
 	const configs = getClientSideSymbologyConfigs();
 	const ConfigsWithMetrics = configs.map((config) => {
 		const metricForThisConfig = getLastPipelineMetricForConfig(config.uuid);
-		const isRunning = pipelineController.getPipeline(config.uuid)?.isRunning();
+		const isRunning = (pipelineController.getPipelines(config.uuid) as ConfigRunner)?.isRunning();
 		return {
 			...config,
 			metric: metricForThisConfig,
