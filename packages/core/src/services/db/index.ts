9122b49dc1c8cd807792db4a3911267b9761624d
Peter Muriuki <pmuriuki98@gmail.com>
2023-01-19 11:48:46 +0300

    Revert "Experimenting with sqlite db as cache for processed facilities"
    This reverts commit d0e1ae7613b9a45d5a9aed30acfecdee95dc0758.

--- packages/core/src/services/db/index.ts
+++
@@ -1,8 +0,0 @@
-import database from 'better-sqlite3';
-import { facilityTable } from './sql';
-
-export function getDb(filePath: string){
-    const db = database(filePath)
-    db.exec(facilityTable)
-    return db
-}
